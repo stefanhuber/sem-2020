d83f581ecdce45c49b3032ff43e0f86b5678a445
Stefan Huber <mail@stefanhuber.at>
2020-10-08 14:54:04 +0200

    added my first app

---
+++ MyFirstApp/app/src/main/java/fhku/myfirstapp/MainActivity.java
@@ -0,0 +1,26 @@
+package fhku.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        EditText textMessage = findViewById(R.id.text_message);
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra("message", textMessage.getText().toString());
+        startActivity(intent);
+    }
+
+}
